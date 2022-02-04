cb8777e829b41af32a4461e78feaf15990f6ac9e
Sangmin Kim <45596238+plantstoen@users.noreply.github.com>
2022-02-04 22:27:16 +0900

    rename: window

--- Main.cpp
+++ Main.cpp
@@ -7,8 +7,6 @@ int main()
 	// Initialize GLFW
 	glfwInit();
 
-
-
 	// Tell GLFW what version of OpenGL we are using 
 	// In this case we are using OpenGL 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -17,10 +15,8 @@ int main()
 	// So that means we only have the modern functions
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-
-
 	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
-	GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 800, "MyFirstOpenGL", NULL, NULL);
 	// Error check if the window fails to create
 	if (window == NULL)
 	{
@@ -31,15 +27,11 @@ int main()
 	// Introduce the window into the current context
 	glfwMakeContextCurrent(window);
 
-
-
 	//Load GLAD so it configures OpenGL
 	gladLoadGL();
 	// Specify the viewport of OpenGL in the Window
 	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
 	glViewport(0, 0, 800, 800);
-
-
 
 	// Specify the color of the background
 	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
@@ -48,16 +40,12 @@ int main()
 	// Swap the back buffer with the front buffer
 	glfwSwapBuffers(window);
 
-
-
 	// Main while loop
 	while (!glfwWindowShouldClose(window))
 	{
 		// Take care of all GLFW events
 		glfwPollEvents();
 	}
-
-
 
 	// Delete window before ending the program
 	glfwDestroyWindow(window);
