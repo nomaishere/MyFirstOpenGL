b81747a2650efad109cd95b90335e2dc19e49843
plantstoen <plantstoen@gmail.com>
2022-07-12 18:06:36 +0900

    Change: Shader annotation

--- Main.cpp
+++ Main.cpp
@@ -65,38 +65,23 @@ GLuint lightIndices[] =
 
 int main()
 {
-	// Initialize GLFW
 	glfwInit();
-
-	// Tell GLFW what version of OpenGL we are using 
-	// In this case we are using OpenGL 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	// Tell GLFW we are using the CORE profile
-	// So that means we only have the modern functions
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // MacOS에서 실행할 때는 추가로 다른 Hint를 줘야함
 
-	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
 	GLFWwindow* window = glfwCreateWindow(width, height, "YoutubeOpenGL", NULL, NULL);
-	// Error check if the window fails to create
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
-	// Introduce the window into the current context
 	glfwMakeContextCurrent(window);
 
-	//Load GLAD so it configures OpenGL
 	gladLoadGL();
-	// Specify the viewport of OpenGL in the Window
-	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
 	glViewport(0, 0, width, height);
 
-
-
-	// Generates Shader object using shaders default.vert and default.frag
 	Shader shaderProgram("default.vert", "default.frag");
 
 
--- shaderClass.h
+++ shaderClass.h
@@ -10,12 +10,15 @@
 
 std::string get_file_contents(const char* filename);
 
+// Vertex Shader客 Fragment Shader肺 捞风绢柳 Shader Program阑 积己 棺 包府
 class Shader
 {
 	public:
 		GLuint ID;
+		// Constructor that build the Shader Program from 2 different shaders
 		Shader(const char* vertexFile, const char* fragmentFile);
 
+		//
 		void Activate();
 		void Delete();
 	private:
